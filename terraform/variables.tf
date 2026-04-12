93f4b5c500731d303a33a413204b0f5e690f9538
codeventurergit <codeventurer+github@gmail.com>
2026-04-12 15:23:46 -0400

    Update variables.tf
    Name the lambda function

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "lambda_function_name" {
   description = "Name of the Lambda function"
   type        = string
-  default     = ""
+  default     = "ip-reputation-service"
 }
 
 variable "dynamodb_table_name" {
