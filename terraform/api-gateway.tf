2e331ff55371c01cf66eda414bb2bd9653f30b75
codeventurergit <codeventurer+github@gmail.com>
2026-04-12 21:32:20 -0400

    Require an api key

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -18,14 +18,15 @@ resource "aws_api_gateway_resource" "ip_lookup" {
 }
 
 ###############################################
-# GET /lookup
+# GET /lookup (API key required)
 ###############################################
 
 resource "aws_api_gateway_method" "get_ip" {
-  rest_api_id   = aws_api_gateway_rest_api.ip_api.id
-  resource_id   = aws_api_gateway_resource.ip_lookup.id
-  http_method   = "GET"
-  authorization = "NONE"
+  rest_api_id      = aws_api_gateway_rest_api.ip_api.id
+  resource_id      = aws_api_gateway_resource.ip_lookup.id
+  http_method      = "GET"
+  authorization    = "NONE"
+  api_key_required = true
 }
 
 ###############################################
@@ -57,8 +58,43 @@ resource "aws_lambda_permission" "api_gateway" {
 ###############################################
 
 resource "aws_api_gateway_deployment" "deploy" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method.get_ip
+  ]
 
   rest_api_id = aws_api_gateway_rest_api.ip_api.id
   stage_name  = "prod"
+}
+
+###############################################
+# API Key
+###############################################
+
+resource "aws_api_gateway_api_key" "ip_api_key" {
+  name    = "ip-reputation-api-key"
+  enabled = true
+}
+
+###############################################
+# Usage Plan
+###############################################
+
+resource "aws_api_gateway_usage_plan" "ip_usage_plan" {
+  name = "ip-reputation-usage-plan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.ip_api.id
+    stage  = aws_api_gateway_deployment.deploy.stage_name
+  }
+}
+
+###############################################
+# Attach API Key to Usage Plan
+###############################################
+
+resource "aws_api_gateway_usage_plan_key" "ip_usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.ip_api_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.ip_usage_plan.id
 }
